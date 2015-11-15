fdd31de601dc93d181b5bf2b1b8d34ec746311e7
eladknol <elad.knoll@nuvo-group.com>
2015-11-15 15:01:47 +0200

    Sampling rate of signal not fixed, now empty

--- Utils/Verification/RTAnalyze/CreateInputStruct.m
+++ Utils/Verification/RTAnalyze/CreateInputStruct.m
@@ -2,7 +2,7 @@ function [ InputStruct ] = CreateInputStruct()
 % CreateInputStruct for 'DecisionFusionLogic.m' 
 
 InputStruct.data=[];
-InputStruct.meta=struct('Filename',[],'Samplerate',1000,'ECGchannels',[1 2 3 4 5 6]...
+InputStruct.meta=struct('Filename',[],'Samplerate',[],'ECGchannels',[1 2 3 4 5 6]...
    ,'MICchannels',[7 8 9 10],'SubjectID',{123456789},'BMIbeforepregnancy',99,'Age',99,'Weekofpregnancy',99);
 
 InputStruct.meta.ChannelsTypes(10).value='MIC';
